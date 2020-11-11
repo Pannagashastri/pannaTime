201749eb1f88b5216913cc62fbb00f218537075f
shashidhar843 <38162619+shashidhar843@users.noreply.github.com>
2020-11-11 18:05:13 +0530

    Updates

---
+++ precache-manifest.48ddf593c199a51a4f49ea294b738f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40cfb1782053e6ce90e6defe3c3a5bf",
+    "url": "/pannaTime/index.html"
+  },
+  {
+    "revision": "a8058cf7963ff30ae16d",
+    "url": "/pannaTime/static/css/main.3d20836d.chunk.css"
+  },
+  {
+    "revision": "a1a0636bc930e7b7e7c7",
+    "url": "/pannaTime/static/js/2.db7586e6.chunk.js"
+  },
+  {
+    "revision": "a8058cf7963ff30ae16d",
+    "url": "/pannaTime/static/js/main.d129af41.chunk.js"
+  },
+  {
+    "revision": "b9b1680610726b06e70e",
+    "url": "/pannaTime/static/js/runtime-main.2331bfc9.js"
+  }
+]);
